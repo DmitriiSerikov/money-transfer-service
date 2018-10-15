33aa35e960557d6dd4cf3801900890f0dae9ff09
Dmitrii Serikov <dmitriiserikov@users.noreply.github.com>
2018-10-15 21:12:14 +0300

    Implemented specification for controllers as IT - Specification covers response codes, payload and headers

--- src/test/groovy/com/github/example/controller/AccountControllerSpec.groovy
+++ src/test/groovy/com/github/example/controller/AccountControllerSpec.groovy
@@ -1,15 +1,21 @@
 package com.github.example.controller
 
+import com.github.example.dto.request.CommandCreateAccount
+import com.github.example.dto.response.AccountData
 import io.micronaut.context.ApplicationContext
-import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.runtime.server.EmbeddedServer
 import org.junit.Test
 import spock.lang.AutoCleanup
 import spock.lang.Shared
 import spock.lang.Specification
 
+/**
+ * Integration test for {@link AccountController}
+ */
 class AccountControllerSpec extends Specification {
 
     @Shared
@@ -20,11 +26,108 @@ class AccountControllerSpec extends Specification {
     HttpClient client = embeddedServer.applicationContext.createBean(HttpClient, embeddedServer.getURL())
 
     @Test
-    def "should return ok when get all accounts"() {
+    def "should return empty collection with 200 status code when accounts not exists yet"() {
         given:
-        HttpResponse response = client.toBlocking().exchange("/accounts")
+        def request = HttpRequest.GET "/accounts"
 
-        expect:
+        when:
+        def response = client.toBlocking().exchange request, Collection.class
+
+        then:
         response.status == HttpStatus.OK
+        response.body().empty
+    }
+
+    @Test
+    def "should return error response with 404 status code when account with specified id doesn't exist"() {
+        given:
+        def request = HttpRequest.GET "/accounts/1"
+
+        when:
+        client.toBlocking().exchange request
+
+        then:
+        def ex = thrown HttpClientResponseException
+        ex.status == HttpStatus.NOT_FOUND
+        ex.message == "Account not exists for id:1"
+    }
+
+    @Test
+    def "should return error with 400 status code when trying to create account with null balance"() {
+        when:
+        createAccount null
+
+        then:
+        def ex = thrown HttpClientResponseException
+        ex.status == HttpStatus.BAD_REQUEST
+        ex.message == "Initial balance cannot be null"
+    }
+
+    @Test
+    def "should return error with 400 status code when trying to create account with negative balance"() {
+        when:
+        createAccount(-100)
+
+        then:
+        def ex = thrown HttpClientResponseException
+        ex.status == HttpStatus.BAD_REQUEST
+        ex.message == "Account balance should be positive or zero"
+    }
+
+    @Test
+    def "should return account data with 201 status code and location header when create account by valid command"() {
+        given:
+        def initialBalance = 500
+        def command = new CommandCreateAccount(initialBalance: 500)
+        def request = HttpRequest.POST "/accounts", command
+
+        when:
+        def response = client.toBlocking().exchange request, AccountData.class
+
+        then:
+        response.status == HttpStatus.CREATED
+        response.body().balance == initialBalance
+        response.header("Location") == "/accounts/" + response.body().id
+    }
+
+    @Test
+    def "should return account data with 200 status code when account with specified id exist"() {
+        given:
+        def initialBalance = 100
+        def accountId = createAccount initialBalance
+        and:
+        def request = HttpRequest.GET "/accounts/" + accountId
+
+        when:
+        def response = client.toBlocking().exchange request, AccountData.class
+
+        then:
+        response.status == HttpStatus.OK
+        response.body().id == accountId
+        response.body().balance == initialBalance
+    }
+
+    @Test
+    def "should return collection of accounts data with 200 status code when accounts exists"() {
+        given:
+        def request = HttpRequest.GET "/accounts"
+        and:
+        createAccount 100
+
+        when:
+        def response = client.toBlocking().exchange request, Collection.class
+
+        then:
+        response.status == HttpStatus.OK
+        !response.body().empty
+    }
+
+    def createAccount(def initialBalance) {
+        def command = new CommandCreateAccount(initialBalance: initialBalance)
+        def request = HttpRequest.POST "/accounts", command
+
+        def response = client.toBlocking().exchange request, AccountData.class
+
+        response.body().id
     }
 }
--- src/test/groovy/com/github/example/controller/TransactionControllerSpec.groovy
+++ src/test/groovy/com/github/example/controller/TransactionControllerSpec.groovy
@@ -1,15 +1,23 @@
 package com.github.example.controller
 
+import com.github.example.dto.request.CommandCreateAccount
+import com.github.example.dto.request.CommandCreateTransaction
+import com.github.example.dto.response.AccountData
+import com.github.example.dto.response.TransactionData
 import io.micronaut.context.ApplicationContext
-import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpStatus
-import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.runtime.server.EmbeddedServer
 import org.junit.Test
 import spock.lang.AutoCleanup
 import spock.lang.Shared
 import spock.lang.Specification
 
+/**
+ * Integration test for {@link TransactionController}
+ */
 class TransactionControllerSpec extends Specification {
 
     @Shared
@@ -17,14 +25,167 @@ class TransactionControllerSpec extends Specification {
     EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer)
     @Shared
     @AutoCleanup
-    RxHttpClient client = embeddedServer.applicationContext.createBean(RxHttpClient, embeddedServer.getURL())
+    HttpClient client = embeddedServer.applicationContext.createBean(HttpClient, embeddedServer.getURL())
+
+    long sourceAccountId
+    long targetAccountId
+
+    def setup() {
+        sourceAccountId = createAccount 100
+        targetAccountId = createAccount 0
+    }
 
     @Test
-    def "should return ok when get all transactions"() {
+    def "should return empty collection with 200 status code when transactions not exists yet"() {
         given:
-        HttpResponse response = client.toBlocking().exchange("/transactions")
+        def request = HttpRequest.GET "/transactions"
 
-        expect:
+        when:
+        def response = client.toBlocking().exchange request, Collection.class
+
+        then:
         response.status == HttpStatus.OK
+        response.body().empty
+    }
+
+    @Test
+    def "should return error response with 404 status code when transaction with specified id doesn't exist"() {
+        given:
+        def request = HttpRequest.GET "/transactions/1"
+
+        when:
+        client.toBlocking().exchange request
+
+        then:
+        def ex = thrown HttpClientResponseException
+        ex.status == HttpStatus.NOT_FOUND
+        ex.message == "Transaction not exists for id:1"
+    }
+
+    @Test
+    def "should return error with 400 status code when trying to create transaction for source account that not exists"() {
+        given:
+        def sourceAccountId = 10
+
+        when:
+        createTransaction sourceAccountId, targetAccountId, 10
+
+        then:
+        def ex = thrown HttpClientResponseException
+        ex.status == HttpStatus.BAD_REQUEST
+        ex.message == "Account not exists for id:10"
+    }
+
+    @Test
+    def "should return error with 400 status code when trying to create transaction for target account that not exists"() {
+        given:
+        def targetAccountId = 10
+
+        when:
+        createTransaction sourceAccountId, targetAccountId, 10
+
+        then:
+        def ex = thrown HttpClientResponseException
+        ex.status == HttpStatus.BAD_REQUEST
+        ex.message == "Account not exists for id:10"
+    }
+
+    @Test
+    def "should return error with 400 status code when trying to create transaction with null amount"() {
+        when:
+        createTransaction sourceAccountId, targetAccountId, null
+
+        then:
+        def ex = thrown HttpClientResponseException
+        ex.status == HttpStatus.BAD_REQUEST
+        ex.message == "Transaction amount cannot be null"
+    }
+
+    @Test
+    def "should return error with 400 status code when trying to create transaction with negative amount"() {
+        when:
+        createTransaction sourceAccountId, targetAccountId, -10
+
+        then:
+        def ex = thrown HttpClientResponseException
+        ex.status == HttpStatus.BAD_REQUEST
+        ex.message == "Transaction amount should be positive"
+    }
+
+    @Test
+    def "should return error with 400 status code when trying to create transaction between same accounts"() {
+        when:
+        createTransaction sourceAccountId, sourceAccountId, 10
+
+        then:
+        def ex = thrown HttpClientResponseException
+        ex.status == HttpStatus.BAD_REQUEST
+        ex.message == "Transactions not allowed between same account id's"
+    }
+
+    @Test
+    def "should return 202 status code and location header when create transaction by valid command"() {
+        given:
+        def command = new CommandCreateTransaction(sourceAccountId: sourceAccountId, targetAccountId: targetAccountId, amount: 10)
+        def request = HttpRequest.POST "/transactions", command
+
+        when:
+        def response = client.toBlocking().exchange request, TransactionData.class
+
+        then:
+        response.status == HttpStatus.ACCEPTED
+        response.header("Location").contains "/transactions/"
+    }
+
+    @Test
+    def "should return transaction data with 200 status code when transaction with specified id exist"() {
+        given:
+        def amount = 100
+        def transactionId = createTransaction sourceAccountId, targetAccountId, amount
+        and:
+        def request = HttpRequest.GET "/transactions/" + transactionId
+
+        when:
+        def response = client.toBlocking().exchange request, TransactionData.class
+
+        then:
+        response.status == HttpStatus.OK
+        response.body().id == transactionId
+        response.body().sourceAccountId == sourceAccountId
+        response.body().targetAccountId == targetAccountId
+        response.body().amount == amount
+    }
+
+    @Test
+    def "should return collection of transactions data with 200 status code when transactions exists"() {
+        given:
+        def request = HttpRequest.GET "/transactions"
+        and:
+        createTransaction sourceAccountId, targetAccountId, 10
+
+        when:
+        def response = client.toBlocking().exchange request, Collection.class
+
+        then:
+        response.status == HttpStatus.OK
+        !response.body().empty
+    }
+
+    def createTransaction(def sourceAccountId, def targetAccountId, def amount) {
+        def command = new CommandCreateTransaction(sourceAccountId: sourceAccountId, targetAccountId: targetAccountId, amount: amount)
+        def request = HttpRequest.POST "/transactions", command
+
+        def response = client.toBlocking().exchange request, TransactionData.class
+
+        response.header("Location") - "/transactions/" as Long
+    }
+
+    def createAccount(def initialBalance) {
+        def command = new CommandCreateAccount(initialBalance: initialBalance)
+        def request = HttpRequest.POST "/accounts", command
+
+        def response = client.toBlocking().exchange request, AccountData.class
+
+        response.body().id
     }
 }
