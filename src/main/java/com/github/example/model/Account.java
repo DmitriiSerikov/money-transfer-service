ae001c91e1bbc265485dd0dec9f5c1f4a352cffa
Dmitrii Serikov <dmitrii_serikov@epam.com>
2018-10-15 17:20:22 +0300

    Created internal models for representing financial domain concepts - Implemented models representing Account and Transaction, both considered immutable in particular implementation - Models has some checks for preventing incosistent state they are also includes methods for mutations

---
+++ src/main/java/com/github/example/model/Account.java
@@ -0,0 +1,61 @@
+package com.github.example.model;
+
+
+import org.modelmapper.internal.util.Assert;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * The {@code Account} class represents bank account in financial domain.
+ * <p>
+ * Accounts are immutable in particular implementation
+ * <p>
+ * The class {@code Account} includes methods for deposit and withdraw money from account balance.
+ */
+public final class Account {
+
+    private static final AtomicLong SEQUENCE = new AtomicLong(0);
+
+    private final long id;
+    private final Instant creationTime;
+    private final BigDecimal balance;
+
+    public Account(final BigDecimal initialBalance) {
+        Assert.notNull(initialBalance, "Initial balance");
+        Assert.isTrue(initialBalance.compareTo(BigDecimal.ZERO) >= 0, "Account balance should be positive or zero");
+
+        this.id = SEQUENCE.incrementAndGet();
+        this.creationTime = Instant.now();
+        this.balance = initialBalance;
+    }
+
+    private Account(Account account, final BigDecimal changedBalance) {
+        Assert.isTrue(changedBalance.compareTo(BigDecimal.ZERO) >= 0, "Account balance should be positive or zero");
+
+        this.id = account.id;
+        this.creationTime = account.creationTime;
+        this.balance = changedBalance;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public Instant getCreationTime() {
+        return creationTime;
+    }
+
+    public Account deposit(final BigDecimal amount) {
+        return new Account(this, balance.add(amount));
+    }
+
+    public Account withdraw(final BigDecimal amount) {
+        return new Account(this, balance.subtract(amount));
+    }
+}
---
+++ src/main/java/com/github/example/model/Transaction.java
@@ -0,0 +1,88 @@
+package com.github.example.model;
+
+import org.modelmapper.internal.util.Assert;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * The {@code Transaction} class represents money transfer in financial domain.
+ * <p>
+ * Transaction are immutable in particular implementation.
+ * <p>
+ * The class {@code Transaction} includes methods for changing status of transaction according to {@link TransactionStatus} values.
+ */
+public final class Transaction {
+
+    private static final AtomicLong SEQUENCE = new AtomicLong(0);
+
+    private final long id;
+    private final Instant creationTime;
+    private final long sourceAccountId;
+    private final long targetAccountId;
+    private final BigDecimal amount;
+    private final TransactionStatus status;
+
+    public Transaction(final long sourceAccountId, final long targetAccountId, final BigDecimal amount) {
+        Assert.notNull(amount, "Transaction amount");
+        Assert.isTrue(amount.compareTo(BigDecimal.ZERO) > 0, "Transaction amount should be positive");
+        Assert.isTrue(sourceAccountId > 0, "Source account id for transaction should be positive");
+        Assert.isTrue(targetAccountId > 0, "Target account id for transaction should be positive");
+        Assert.isTrue(sourceAccountId != targetAccountId, "Transactions not allowed between same account id's");
+
+        this.id = SEQUENCE.incrementAndGet();
+        this.creationTime = Instant.now();
+        this.status = TransactionStatus.PENDING;
+        this.sourceAccountId = sourceAccountId;
+        this.targetAccountId = targetAccountId;
+        this.amount = amount;
+    }
+
+    private Transaction(final Transaction transaction, final TransactionStatus changedStatus) {
+        this.id = transaction.id;
+        this.creationTime = transaction.creationTime;
+        this.status = changedStatus;
+        this.sourceAccountId = transaction.sourceAccountId;
+        this.targetAccountId = transaction.targetAccountId;
+        this.amount = transaction.amount;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Instant getCreationTime() {
+        return creationTime;
+    }
+
+    public long getSourceAccountId() {
+        return sourceAccountId;
+    }
+
+    public long getTargetAccountId() {
+        return targetAccountId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public TransactionStatus getStatus() {
+        return status;
+    }
+
+    public Transaction executed() {
+        return new Transaction(this, TransactionStatus.SUCCESS);
+    }
+
+    public Transaction failed() {
+        return new Transaction(this, TransactionStatus.FAILED);
+    }
+
+    public enum TransactionStatus {
+        PENDING,
+        SUCCESS,
+        FAILED
+    }
+}
