062045d493a8f22fb43f438fc097d29cd5b823de
Dmitrii Serikov <dmitrii_serikov@epam.com>
2018-10-15 17:45:54 +0300

    Implemented DAOs according to spec backed by CHM as in-memory storage - Implemented therad-safe logic for CRU operations - Update operation implemented with locks for preventing race conditions and consistency - Implemneted logic for pessimistic locking

--- src/main/java/com/github/example/dao/impl/InMemoryAccountDaoImpl.java
+++ src/main/java/com/github/example/dao/impl/InMemoryAccountDaoImpl.java
@@ -1,40 +1,80 @@
 package com.github.example.dao.impl;
 
 import com.github.example.dao.AccountDao;
+import com.github.example.exception.EntityAlreadyExistsException;
+import com.github.example.exception.EntityNotFoundException;
+import com.github.example.holder.LockHolder;
 import com.github.example.model.Account;
+import org.modelmapper.internal.util.Assert;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
+import static java.util.Collections.unmodifiableCollection;
 
+@Singleton
 public class InMemoryAccountDaoImpl implements AccountDao {
+
+    private final Map<Long, Account> storage = new ConcurrentHashMap<>();
+    private final LockHolder lockHolder;
+
+    @Inject
+    public InMemoryAccountDaoImpl(final LockHolder lockHolder) {
+        this.lockHolder = lockHolder;
+    }
 
     @Override
     public Collection<Account> findAll() {
-        return null;
+        return unmodifiableCollection(storage.values());
     }
 
     @Override
-    public Account insert(Account account) {
-        return null;
+    public Account insert(final Account account) {
+        Assert.notNull(account);
+
+        return storage.compute(account.getId(), (id, previousValue) -> {
+            if (previousValue != null) {
+                throw new EntityAlreadyExistsException("Account already exists for id:" + id);
+            }
+            return account;
+        });
     }
 
     @Override
-    public Account getBy(long accountId) {
-        return null;
+    public Account getBy(final long accountId) {
+        return Optional.ofNullable(storage.get(accountId))
+                .orElseThrow(() -> new EntityNotFoundException("Account not exists for id:" + accountId));
     }
 
     @Override
-    public void update(Account account) {
+    public void update(final Account account) {
+        Assert.notNull(account);
 
+        final long accountId = account.getId();
+
+        lockBy(accountId);
+        try {
+            storage.put(accountId, account);
+        } finally {
+            unlockBy(accountId);
+        }
     }
 
     @Override
-    public void lockBy(long accountId) {
-
+    public void lockBy(final long accountId) {
+        lockHolder.acquire(getLockId(accountId));
     }
 
     @Override
-    public void unlockBy(long accountId) {
+    public void unlockBy(final long accountId) {
+        lockHolder.release(getLockId(accountId));
+    }
 
+    private String getLockId(final long accountId) {
+        return "Account_" + accountId;
     }
 }
--- src/main/java/com/github/example/dao/impl/InMemoryTransactionDaoImpl.java
+++ src/main/java/com/github/example/dao/impl/InMemoryTransactionDaoImpl.java
@@ -1,45 +1,90 @@
 package com.github.example.dao.impl;
 
 import com.github.example.dao.TransactionDao;
+import com.github.example.exception.EntityAlreadyExistsException;
+import com.github.example.exception.EntityNotFoundException;
+import com.github.example.holder.LockHolder;
 import com.github.example.model.Transaction;
+import org.modelmapper.internal.util.Assert;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
+import static com.github.example.model.Transaction.TransactionStatus.PENDING;
+import static java.util.Collections.unmodifiableCollection;
 
+@Singleton
 public class InMemoryTransactionDaoImpl implements TransactionDao {
+
+    private final Map<Long, Transaction> storage = new ConcurrentHashMap<>();
+    private final LockHolder lockHolder;
+
+    @Inject
+    public InMemoryTransactionDaoImpl(final LockHolder lockHolder) {
+        this.lockHolder = lockHolder;
+    }
 
     @Override
     public Collection<Transaction> findAll() {
-        return null;
+        return unmodifiableCollection(storage.values());
     }
 
     @Override
     public Collection<Transaction> findPending(int limit) {
-        return null;
+        return findAll().stream()
+                .filter(transaction -> PENDING.equals(transaction.getStatus()))
+                .limit(limit)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public Transaction insert(Transaction transaction) {
-        return null;
+    public Transaction insert(final Transaction transaction) {
+        Assert.notNull(transaction);
+
+        return storage.compute(transaction.getId(), (id, previousValue) -> {
+            if (previousValue != null) {
+                throw new EntityAlreadyExistsException("Transaction already exists for id:" + id);
+            }
+            return transaction;
+        });
     }
 
     @Override
-    public Transaction getBy(long transactionId) {
-        return null;
+    public Transaction getBy(final long transactionId) {
+        return Optional.ofNullable(storage.get(transactionId))
+                .orElseThrow(() -> new EntityNotFoundException("Transaction not exists for id:" + transactionId));
     }
 
     @Override
-    public void update(Transaction transaction) {
+    public void update(final Transaction transaction) {
+        Assert.notNull(transaction);
 
+        final long transactionId = transaction.getId();
+
+        lockBy(transactionId);
+        try {
+            storage.put(transactionId, transaction);
+        } finally {
+            unlockBy(transactionId);
+        }
     }
 
     @Override
-    public void lockBy(long transactionId) {
-
+    public void lockBy(final long transactionId) {
+        lockHolder.acquire(getLockId(transactionId));
     }
 
     @Override
-    public void unlockBy(long transactionId) {
+    public void unlockBy(final long transactionId) {
+        lockHolder.release(getLockId(transactionId));
+    }
 
+    private String getLockId(final long transactionId) {
+        return "Transaction_" + transactionId;
     }
 }
